416e094979e20b96e6e53209547242e93eabd204
chisombiri <chisombiri@gmail.com>
2022-04-03 14:17:21 +0100

    code clean-up

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -8,15 +8,15 @@ const Register = ({ onRouteChange }) => {
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                     <legend className="f2 fw6 ph0 mh0">Register</legend>
                     <div className="mt3">
-                        <label className="db fw6 lh-copy f6" for="name">Username</label>
+                        <label className="db fw6 lh-copy f6" htmlFor="name">Username</label>
                         <input className="pa2 input-reset b--black-30 ba bg-transparent hover-bg-black hover-white w-100" type="name" name="name"  id="name"/>
                     </div>
                     <div className="mt3">
-                        <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+                        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                         <input className="pa2 input-reset b--black-30 ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
                     </div>
                     <div className="mv3">
-                        <label className="db fw6 lh-copy f6" for="password">Password</label>
+                        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                         <input className="b pa2 input-reset b--black-30 ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
                     </div>
                     </fieldset>
