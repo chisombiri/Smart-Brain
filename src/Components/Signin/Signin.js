fcd728c04582f987c5dfacd0cab539ea333fbe65
chisombiri <chisombiri@gmail.com>
2022-04-08 13:43:48 +0100

    text block for registering

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -79,7 +79,8 @@ class Signin extends React.Component{
                         />
                         </div>
                         <div className="lh-copy mt3">
-                        <p onClick={() => onRouteChange('register')}  className="f6 link dim black db pointer">Register</p>
+                        <h5>You should Register first if this is your first try:</h5>
+                        <p onClick={() => onRouteChange('register')}  className="f6 link underline dim black db pointer">Register</p>
                         </div>
                     </form>
                 </main>
